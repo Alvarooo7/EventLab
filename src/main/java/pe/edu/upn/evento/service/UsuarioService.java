7605f88cd2f3ddb68a1ce29f8906327e763711bd
Alvaro <alvarofranco2020@gmail.com>
2019-12-11 11:35:42 -0500

    Primera Version con todas la funcionalidades

---
+++ src/main/java/pe/edu/upn/evento/service/UsuarioService.java
@@ -0,0 +1,11 @@
+package pe.edu.upn.evento.service;
+
+import java.util.Optional;
+
+import pe.edu.upn.evento.model.entity.Usuario;
+
+public interface UsuarioService extends CrudService<Usuario, Integer> {
+	Optional<Usuario> findByUsername(String username) throws Exception;
+	
+	Optional<Usuario> findByEmail(String email) throws Exception;
+}
